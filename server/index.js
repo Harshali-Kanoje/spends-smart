3a37eac3df45f80751ad5a5053421410e8b8c5ee
Harshali Kanoje <harshalikanoje@gmail.com>
2023-12-17 22:57:20 -0800

    some changes are made

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotevn from 'dotenv'
+import path from 'path'
 dotevn.config()
 import { postApiTransaction ,getApiTransaction ,postAppSignup ,postAppLogin} from './controlller/Transaction.js';
 
 const app = express();
 
 app.use(express.json())
+
+const __dirname = path.resolve();
 
 const connectDB = async () => {
     const connection = await mongoose.connect(process.env.MONGODB_URI)
@@ -25,6 +28,14 @@ app.get('/api/transactions', getApiTransaction)
 app.post('/api/signup' ,postAppSignup)
 
 app.post('/api/login' ,postAppLogin)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
